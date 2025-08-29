132f607ccf22f1b8af3db3def7ee47da280b0765
acgrp103 <acgrp103@gmail.com>
2025-08-29 15:33:54 +0900

    Sync

---
+++ java-adv1/java-adv1/src/threadEx/sync/BankAccountV3.java
@@ -0,0 +1,43 @@
+package threadEx.sync;
+
+import static util.ThreadUtils.sleep;
+import static utilEx.MyLogger.log;
+
+public class BankAccountV3 implements BankAccount {
+
+    private int balance;
+
+    public BankAccountV3(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+        //잔고가 출금액보다 적으면, 진행x
+
+        //====임계 영역 시작====
+        synchronized (this) {
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false;
+            }
+
+            //잔고가 출금액보다 많으면 진행o
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            sleep(1000);//출금에 걸리는 시간
+            balance = balance - amount;
+            log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        }
+        //====임계 영역 종료====
+
+        log("거래 종료: ");
+        return false;
+    }
+
+    @Override
+    public synchronized int getBalance() {
+        return balance;
+    }
+}
---
+++ java-adv1/java-adv1/src/threadEx/sync/WithDrawTask.java
@@ -0,0 +1,17 @@
+package threadEx.sync;
+
+public class WithDrawTask implements Runnable {
+
+    private BankAccount account;
+    private int amount;
+
+    public WithDrawTask(BankAccount account, int amount) {
+        this.account = account;
+        this.amount = amount;
+    }
+
+    @Override
+    public void run() {
+        account.withdraw(amount);
+    }
+}
